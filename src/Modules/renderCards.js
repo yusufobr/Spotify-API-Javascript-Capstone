890fa1e61689e8d0a5994cedc97c3071aabc59de
yusufobr <yousarsif80@gmail.com>
2023-11-25 16:42:39 +0100

    add lazy load

--- src/Modules/renderCards.js
+++ src/Modules/renderCards.js
@@ -1,27 +1,36 @@
-import { addLike, fetchLikes } from './addLike.js';
-import itemsCounter from './itemCounter.js';
+import { addLike, fetchLikes } from "./addLike.js";
+import itemsCounter from "./itemCounter.js";
 
-const cards = document.getElementById('cards');
+const cards = document.getElementById("cards");
 
 const renderCards = (array) => {
-  let htmlToAppend = '';
+  let htmlToAppend = "";
   array.forEach((data) => {
+    let artistsAlias = "";
 
-    let artistsAlias = ''
-
-    if(data?.artists === undefined) {
-      artistsAlias = 'Unknown Artist'
+    if (data?.artists === undefined) {
+      artistsAlias = "Unknown Artist";
     } else {
-      artistsAlias = data?.artists[0]?.alias || 'Unknown Artist'
+      artistsAlias = data?.artists[0]?.alias || "Unknown Artist";
     }
 
     const card = `
-          <div id="${data.key}" class="flex flex-col gap-2 p-4 h-full rounded-2xl bg-[#1C1F26] drop-shadow-md border-2 border-[#a8b3cf33;] hover:border-[#1ED760;] card-color">
+          <div id="${
+            data.key
+          }" class="flex flex-col gap-2 p-4 h-full rounded-2xl bg-[#1C1F26] drop-shadow-md border-2 border-[#a8b3cf33;] hover:border-[#1ED760;] card-color">
             <div class="w-full h-[250px] rounded-lg overflow-hidden image-container">
-              <img class="h-[250px] w-auto rounded-lg mx-auto" src="${data.images?.coverart || data.images?.background || `https://dummyimage.com/400x400/000/1aff00&text=${data.title}`}" />
+              <img loading="lazy" class="h-[250px] w-auto rounded-lg mx-auto" src="${
+                data.images?.coverart ||
+                data.images?.background ||
+                `https://dummyimage.com/400x400/000/1aff00&text=${data.title}`
+              }" />
             </div>
             <div class="flex flex-col gap-2 justify-between">
-              <a href="${data.url}" class="font-semibold text-lg capitalize custom-title cursor-pointer hover:text-[#1ED760]">${data.title.length < 28 ? data.title : `${data.title.substring(0, 25)}...`}</a>
+              <a href="${
+                data.url
+              }" class="font-semibold text-lg capitalize custom-title cursor-pointer hover:text-[#1ED760]">${
+      data.title.length < 28 ? data.title : `${data.title.substring(0, 25)}...`
+    }</a>
               <h5 class="text-[#777] -mt-2 uppercase">${artistsAlias}</h5>
               <div class="flex flex-col gap-1 justify-between">
                 <div class="flex justify-between p-1 items-center">
@@ -50,18 +59,18 @@ const renderCards = (array) => {
       const likeNbr = document.getElementById(`like-${data.key}`);
 
       const item = showLikes.find((item) => item.item_id === data.key);
-      likeNbr.innerHTML = item ? `${item.likes} likes` : 'No like yet';
+      likeNbr.innerHTML = item ? `${item.likes} likes` : "No like yet";
     });
   });
 
   array.forEach((data) => {
     const { key } = data;
     const likeNbr = document.getElementById(`like-${key}`);
-    const likeBtn = likeNbr.parentElement.querySelector('img');
+    const likeBtn = likeNbr.parentElement.querySelector("img");
 
-    likeBtn.addEventListener('click', () => {
+    likeBtn.addEventListener("click", () => {
       addLike(key, likeNbr);
-      likeBtn.setAttribute('src', './assets/adoreRed.svg');
+      likeBtn.setAttribute("src", "./assets/adoreRed.svg");
     });
   });
   itemsCounter();
